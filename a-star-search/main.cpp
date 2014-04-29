240dcda50fa249b055875527ec5398fa5695248d
Gabriel Bretschner <software@kanedo.net>
2014-04-29 08:06:09 +0200

    Initial Commit

---
+++ a-star-search/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  a-star-search
+//
+//  Created by Gabriel Bretschner on 29.04.14.
+//  Copyright (c) 2014 Gabriel Bretschner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
